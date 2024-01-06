b34982f22617554b748f372e03ad9d89b718bae4
theomorphic <FelixMK@yandex.ru>
2024-01-07 03:05:14 +0500

    feat: add dark theme

--- script.js
+++ script.js
@@ -8,4 +8,29 @@ function settingsMenuToggle(){
 
 darkBtn.onclick = function(){
 	darkBtn.classList.toggle("dark-btn-on");
+
+	document.body.classList.toggle("dark-theme"); //дает всему body класс
+
+
+	if(localStorage.getItem("theme") == "light"){
+		localStorage.setItem("theme", "dark");
+	}
+	else{
+		localStorage.setItem("theme", "light");
+
+	}
+
 };
+
+
+if(localStorage.getItem("theme") == "light"){
+	darkBtn.classList.remove("dark-btn-on");
+	document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme") == "dark"){
+	darkBtn.classList.add("dark-btn-on");
+	document.body.classList.add("dark-theme");
+}
+else{
+	localStorage.setItem("theme", "light");
+}
